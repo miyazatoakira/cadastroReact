94c5b932a55a0aa6266dfb1516df5e3726e8542c
miyazatoakira <augusto.akira08@hotmail.com>
2024-06-18 09:45:29 -0300

    Responsividade Cadastro.jsx Corrigida

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Interfaces Web II - Atividade de Tela de Login usando REACT
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Principal aprendizado se deu no uso de Context API
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -19,27 +19,26 @@ const Cadastro = () => {
   return (
     <div>
       <div className="container cadastro mt-5">
-        <form className="row text-center was-validated justify-content-center py-4" onSubmit={handleSubmit} novalidated>
-          <div className="col-10 ">
+        <form className="row text-center need-validation justify-content-center py-4" onSubmit={handleSubmit} novalidated>
+          <div className="col-12 col-md-10">
             <h1 className="h1 Home-title text-start">Cadastre-se</h1>
             <div className="line"></div>
           </div>
-          <div className="col-5 ">
+          <div className="col-12 col-md-5">
             <Form formClass="my-4" id="firstName" msg="Insira Primeiro Nome*" type="text" />
             <Form formClass="my-4" id="email" msg="Insira seu E-mail*" type="email" onChange={(e) => setEmail(e.target.value)} />
             <Form formClass="my-4" id="password" msg="Insira senha*" type="password" />
             <Checkbox type="radio" checkboxClass="radioMale text-start" nameIn="Sexo" msg="Masculino" value="Masculino" id="radioMale" />
-
           </div>
-          <div className="col-5 ">
+          <div className="col-12 col-md-5">
             <Form formClass="my-4" id="lastName" msg="Insira Sobrenome" type="text" />
             <Form formClass="my-4" id="telefone" msg="Insira seu telefone" type="tel" />
             <Form formClass="my-4" id="password" msg="Confirme a senha*" type="password" />
             <div className="row g-5 mb-5">
-              <div className="col-6  ">
+              <div className="col-md-6 col-12 ">
                 <Checkbox type="radio" checkboxClass="radioFemale text-start" nameIn="Sexo" msg="Feminino" value="Feminino" id="radioFemale" />
               </div>
-              <div className="col-6  ">
+              <div className="col-12 col-md-6  ">
                 <Checkbox type="radio" checkboxClass="radioNone text-start" nameIn="Sexo" msg="Prefiro não dizer" value="None" id="radioNone" />
               </div>
             </div>
@@ -49,13 +48,13 @@ const Cadastro = () => {
             <div className="col-11 ">
               <Form formClass="my-4" id="adress" msg="Endereço" type="text" />
             </div>
-            <div className="col-6">
+            <div className="col-11 col-md-6 mt-2">
               <Form formClass="" id="city" msg="Cidade" type="text" />
             </div>
-            <div className="col-3 ">
+            <div className="col-6 mt-2 col-md-3 ">
               <Form formClass="" id="cep" msg="CEP" type="number" />
             </div>
-            <div className="col-2">
+            <div className="col-5 col-md-2 mt-2">
               <select className="form-select text-upperacase" aria-label="Default select example">
                 <option className="text-uppercase" selected>UF</option>
                 <option className="text-uppercase" value="ac">ac</option>
@@ -87,7 +86,7 @@ const Cadastro = () => {
             <div className="col-10  mt-3">
               <Checkbox type="checkbox" nameIn="terns" checkboxClass="text-start" msg="Concordo com os termos" value="agree" id="terms" />
             </div>
-            <div className="col-12">
+            <div className="col-12 mt-2">
               <button  className="btn btn-lg btn-outline-success" type="submit">Cadastrar-se</button>
             </div>
           </div>

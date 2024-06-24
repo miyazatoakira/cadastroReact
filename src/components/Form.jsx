04db6796076351e385aa1d49248d34a03b47c9e7
miyazatoakira <augusto.akira08@hotmail.com>
2024-06-24 15:44:45 -0300

    WORKING Toggle Password mode Working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 
 const Form = ({ type, id, msg, onChange, formClass, isReq, icon, onClick }) => {
 
-    return (
-        <div>
-            <div className={`form-floating ${formClass}`}>
-                <input
-
-                    type={type}
-                    className="form-control"
-                    id={id}
-                    placeholder={msg}
-                    onChange={onChange}
-
-                />
-                <button onClick={onClick} className={icon}></button>
-                <label className="ms-2" htmlFor={id}>{msg}</label>
-            </div>
+  return (
+    <div>
+      <div className={`form-floating ${formClass}`}>
+        <div className="input-group">
+          <input
+            type={type}
+            className="form-control"
+            id={id}
+            placeholder={msg}
+            onChange={onChange}
+          />
+          {icon && (
+            <span className="input-group-text" onClick={onClick}>
+              <i className={icon}></i>
+            </span>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Form;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import viteLogo from '../vite.svg';
 import logo from '../logo.svg';
 import Form from "../components/Form";
 import Checkbox from '../components/Checkbox';
 import { Link, useNavigate } from 'react-router-dom';
 import { useEmailName } from '../EmailNameContext';
-
 
 const Home = () => {
   const [name, setName] = useState('');
@@ -15,86 +14,70 @@ const Home = () => {
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
   const [ToastColor, setToastColor] = useState("");
-  const [showPass, setShowPass] = useState(false)
+  const [showPass, setShowPass] = useState(false);
 
-  let eyeState = "bi-eye-slash"
-  let passwordType = "password"
+  const togglePasswordMode = () => {
+    setShowPass(!showPass);
+  };
 
-
-  const togglePasswordMode = () =>{
-    showPass = !showPass;
-    if(showPass){
-      eyeState = "bi-eye";
-      passwordType = "text"
-    }
-    else{
-      eyeState = "bi-eye-slash"
-      passwordType = "password"
-    }
-  }
-
+  let eyeState = showPass ? "bi-eye" : "bi-eye-slash";
+  let passwordType = showPass ? "text" : "password";
 
   const handleSubmit = (event) => {
-    console.log(name, password)
-      if (password === "1234") {
-        let email = `${name}@gmail.com`;
-        event.preventDefault();
-            // Define o nome no contexto
-        setUserDetails(email, name);
-        // Navega para a página de confirmação de cadastro
-        navigate('/ConfCadastro');
-    }
-    else {
-        setToastMessage("Usuário Inválido");
-        setToastColor("text-bg-danger");
+    event.preventDefault();
+    console.log(name, password);
+
+    if (password === "1234") {
+      let email = `${name}@gmail.com`;
+      setUserDetails(email, name);
+      navigate('/ConfCadastro');
+    } else {
+      setToastMessage("Usuário Inválido");
+      setToastColor("text-bg-danger");
     }
 
-    setShowToast(true)
-  }
-
-
+    setShowToast(true);
+  };
 
   return (
     <div>
       <div className="container">
-          <div aria-live="polite" aria-atomic="true" className="d-flex justify-content-center align-items-center w-100">
-              <div className={`toast align-items-center ${ToastColor} border-0 ${showToast ? "show" : ""}`}>
-                  <div className="d-flex">
-                      <div className="toast-body">
-                          {toastMessage}
-                      </div>
-                      <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={() => setShowToast(false)}></button>
-                  </div>
+        <div aria-live="polite" aria-atomic="true" className="d-flex justify-content-center align-items-center w-100">
+          <div className={`toast align-items-center ${ToastColor} border-0 ${showToast ? "show" : ""}`}>
+            <div className="d-flex">
+              <div className="toast-body">
+                {toastMessage}
               </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-5 bemvindo pt-5 text-center">
-                <h1 className="h1 Home-title text-center mt-4">Bem-vindo </h1>
-                <p className="subtitle text-center mt-5">Cadastre sua conta e venha fazer parte da nossa equipe</p>
-                <Link to="/Cadastro" className="btn btn-outline-dark p-3 px-4  rounded-pill">Inscreva-se</Link>
-                <p className="subtitle text-center mt-5">Ou entre</p>
-                <div className="social-icons mt-2 ">
-                    <i className="bi bi-google me-3 rounded-circle "></i>
-                    <i className="bi bi-facebook me-3 rounded-circle "></i>
-                    <i className="bi bi-linkedin rounded-circle "></i>
-                </div>
+              <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={() => setShowToast(false)}></button>
             </div>
-            <form onSubmit={handleSubmit}  className="login col-7 text-center">
-              <h2 className="h2 mt-5 Home-title text-start ms-5">
-                  <img src={viteLogo} alt="Vite Logo" className='me-4' />
-                  ENTRAR <img className='react-logo' src={logo} alt="React Logo" style={{width: "2em"}} /></h2>
-                <Form icon="bi bi-person-fill" type="text" formClass="my-5 mx-5 me-4" msg="Usuário" id="user" onChange={(e) => setName(e.target.value)}  />
-                <Form onClick={togglePasswordMode} icon={`bi ${eyeState}`} type={passwordType} formClass="mb-2 mx-5 me-4" msg="Senha" id="password" onChange={(e) => setPassword(e.target.value)} />
-
-                <Checkbox type="checkbox" checkboxClass="mx-5 mb-4 text-start" msg="Lembre de mim" value="rememberMeChecked" id="rememberMe" />
-                <button className="btn btn-primary mt-2 mb-5 btn-lg rounded-pill w-75" >Entrar</button>
-            </form>
           </div>
-
-
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-5 bemvindo pt-5 text-center">
+            <h1 className="h1 Home-title text-center mt-4">Bem-vindo</h1>
+            <p className="subtitle text-center mt-5">Cadastre sua conta e venha fazer parte da nossa equipe</p>
+            <Link to="/Cadastro" className="btn btn-outline-dark p-3 px-4 rounded-pill">Inscreva-se</Link>
+            <p className="subtitle text-center mt-5">Ou entre</p>
+            <div className="social-icons mt-2">
+              <i className="bi bi-google me-3 rounded-circle"></i>
+              <i className="bi bi-facebook me-3 rounded-circle"></i>
+              <i className="bi bi-linkedin rounded-circle"></i>
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className="login col-7 text-center">
+            <h2 className="h2 mt-5 Home-title text-start ms-5">
+              <img src={viteLogo} alt="Vite Logo" className='me-4' />
+              ENTRAR <img className='react-logo' src={logo} alt="React Logo" style={{ width: "2em" }} />
+            </h2>
+            <Form icon="bi bi-person-fill" type="text" formClass="my-5 mx-5 me-4" msg="Usuário" id="user" onChange={(e) => setName(e.target.value)} />
+            <Form onClick={togglePasswordMode} icon={`bi ${eyeState}`} type={passwordType} formClass="mb-2 mx-5 me-4" msg="Senha" id="password" onChange={(e) => setPassword(e.target.value)} />
+            <Checkbox type="checkbox" checkboxClass="mx-5 mb-4 text-start" msg="Lembre de mim" value="rememberMeChecked" id="rememberMe" />
+            <button className="btn btn-primary mt-2 mb-5 btn-lg rounded-pill w-75">Entrar</button>
+          </form>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
